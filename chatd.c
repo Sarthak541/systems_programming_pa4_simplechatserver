26fe62c40e7efb4dfe796ed77c3f0d9c3fc92415
Sarthak <sr.sarthak.talukdar@gmail.com>
2026-05-01 15:47:09 -0400

    chagnes

--- chatd.c
+++ chatd.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
-#define PORT 8080 //for testing purposes use localhost
+
 int main(int argc, char* argv[]) {
     if (argc != 2) {
         fprintf(stderr, "Must have 1 argument\n");
         exit(EXIT_FAILURE);
     }
-    int socket_fd;   
 }

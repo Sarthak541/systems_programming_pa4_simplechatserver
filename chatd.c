c9b4960387001d0e0a71a1a8ab47e43c7a6a7b39
Yaswanth Gosukonda <sci.his.books@gmail.com>
2026-05-02 12:22:00 -0400

    fixed parsing

--- chatd.c
+++ chatd.c
@@ -329,23 +329,39 @@ int is_valid_message(const char *s){
 }
 
 int main(int argc, char* argv[]) {
-    if (argc != 3) {
-        fprintf(stderr, "Write: %s, <host> <port>\n", argv[0]);
+    if (argc != 2) {
+        fprintf(stderr, "Must have 1 arugment\n");
         exit(EXIT_FAILURE);
     }
+    //parsing host:port from argv[1]
+    char buffer[256];
+    strncpy(buffer, argv[1], sizeof(buffer));
+    buffer[255] = '\0';
+    //since colon separates host from port, figure out where the colon occurs
+    char *colon = strchr(buffer, ':');
+    if(colon == NULL){
+        fprintf(stderr, "Invalid format. Expected host:port\n");
+        exit(EXIT_FAILURE);
+    }
+    *colon = '\0';
+    char *host = buffer;
+    char *port = colon + 1;
+    if(*port == '\0'){
+        fprintf(stderr, "Need port\n");
+        exit(EXIT_FAILURE);
+    }
+
     struct addrinfo hints;
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
 
     struct addrinfo* list;
-    char* host = argv[1];
-    char* port = argv[2];
     int r = getaddrinfo(host, port, &hints, &list);
 
     if (r!=0) {
         //handles error specifically for getaddrinfo
-        gai_strerror(r);
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(r));
         exit(EXIT_FAILURE);
     }
     
@@ -386,5 +402,4 @@ int main(int argc, char* argv[]) {
         fprintf(stderr, "Unable to bind\n");
         exit(EXIT_FAILURE);
     }
-
 }
